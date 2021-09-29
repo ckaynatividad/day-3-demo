84dc4f88ffeee43a56c034761de9f9d0d375e4f3
ckaynatividad <natividad.ckay@gmail.com>
2021-09-29 11:04:16 -0700

    notes

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-// import functions and grab DOM elements
+// strings 
+console.log("hello world");
+// numbers
+console.log(1+1);
+//booloeans
+console.log(false);
+console.log(true);
 
-// initialize global state
+//emptyness
+console.log(undefined);
+console.log(null);
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+//variables
+const PI= 3.145926;
+//cannot reassign constants
+console.log(PI);
+console.log(`The area of a circle w radius 2 is: ${PI * 2 *2}`);
+
+//template literal
+let myName = 'ckay';
+let catName = 'cardamon';
+//use doub quotes if string contains single
+//let myString = 'my cat\'s name is cardamon';
+console.log(`my name is ${myName} and my dog's name is ${catName}`)
+
+// variable rules - variable names can contain
+// a-z
+// A-Z
+// $ _
+// 1-9
+// cannot start w a number
+// constant are w ALL_CAPS and snake case 
+// arrays 
+let myList = [1,2,3,4];
+console.log(myList)
+console.log(nums);
+console.log(names);
+console.log(mixed);
